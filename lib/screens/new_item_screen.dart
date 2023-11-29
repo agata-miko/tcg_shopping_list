a84d8181bb133dde8bfe5fbed7081b26eb734e8e
mikol <mikolajek.a@gmail.com>
2023-11-29 21:23:50 +0100

    Form and text form field added to new_item_screen (207)

--- lib/screens/new_item_screen.dart
+++ lib/screens/new_item_screen.dart
@@ -12,9 +12,23 @@ class _NewItemScreenState extends State<NewItemScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Add a new item')),
-      body: const Padding(
-        padding: EdgeInsets.all(12),
-        child: Text('The form'),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return 'Demo...';
+                } ,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
