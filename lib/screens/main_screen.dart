2c05f97526c7e638e3816739d75ed5d7e41c4552
mikol <mikolajek.a@gmail.com>
2023-12-08 11:28:10 +0100

    Data fetched from firebase and transformed to GroceryItems on main screen (223)

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:tcg_shopping_list/data/categories.dart';
 import 'package:tcg_shopping_list/models/grocery_item.dart';
 import 'package:tcg_shopping_list/screens/new_item_screen.dart';
 
@@ -10,21 +14,48 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-tcg-shopping-list-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData =
+        json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere((catItem) => catItem.value.name == item.value['category'])
+          .value;
+      _loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
 
   void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
+    await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItemScreen(),
       ),
     );
 
-    if (newItem == null) {
-      return;
-    }
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
@@ -36,37 +67,37 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Your Groceries'),
-          actions: [
-            IconButton(
-              onPressed: _addItem,
-              icon: const Icon(Icons.add),
-            ),
-          ],
-        ),
-        body: _groceryItems.isEmpty
-            ? const Center(child: Text('No items yet'))
-            : ListView.builder(
-    itemCount: _groceryItems.length,
-        itemBuilder: (context, index)
-    =>
-        Dismissible(
-          key: ValueKey(_groceryItems[index].id),
-          background: Container(color: Colors.red.withOpacity(0.2),),
-          onDismissed: (direction) {_removeItem(_groceryItems[index]);},
-          child: ListTile(
-            title: Text(_groceryItems[index].name),
-            leading: Icon(
-              Icons.square,
-              color: _groceryItems[index].category.color,
-            ),
-            trailing: Text((_groceryItems[index].quantity).toString()),
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
           ),
-        )
-    ,
-    )
-    ,
+        ],
+      ),
+      body: _groceryItems.isEmpty
+          ? const Center(child: Text('No items yet'))
+          : ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: (context, index) => Dismissible(
+                key: ValueKey(_groceryItems[index].id),
+                background: Container(
+                  color: Colors.red.withOpacity(0.2),
+                ),
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+                child: ListTile(
+                  title: Text(_groceryItems[index].name),
+                  leading: Icon(
+                    Icons.square,
+                    color: _groceryItems[index].category.color,
+                  ),
+                  trailing: Text((_groceryItems[index].quantity).toString()),
+                ),
+              ),
+            ),
     );
   }
 }
